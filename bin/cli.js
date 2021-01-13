97cef377c45a541ec64ac504c071cd65432a4f6b
n2ptune <n2ptune@outlook.com>
2021-01-14 00:34:19 +0900

    init

---
+++ bin/cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const handler = require('serve-handler')
+const http = require('http')
+const path = require('path')
+const PORT = 4500
+
+const server = http.createServer((request, response) => {
+  return handler(request, response, {
+    public: path.resolve(__dirname, '../', 'dist')
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`started server at http://localhost:${PORT}`)
+})
